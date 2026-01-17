8c52b945d98980f1993666a5c42828e9421c3855
Ashique <ashique12009@gmail.com>
2026-01-17 21:22:26 +0200

    initial commit

---
+++ wp-content/themes/green-hotel-booking/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Custom Theme</title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+
+<h1>হ্যালো! এটা আমার Custom WordPress Theme 🎉</h1>
+
+</body>
+</html>
